e6891b13555fdc701b0afc39f3bf380862fa63d6
FIorian <florian.gaeremynck@gmail.com>
2025-11-03 16:03:28 +0100

    adjsut build tools to what I'm used to

--- makeSKIRT.sh
+++ makeSKIRT.sh
@@ -29,9 +29,9 @@ fi
 
 # Generate the build files
 $CMAKEPATH -E make_directory ../release
-$CMAKEPATH -E chdir ../release $CMAKEPATH -DCMAKE_BUILD_TYPE:STRING=Release -L ../git
+$CMAKEPATH -E chdir ../release $CMAKEPATH -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE:STRING=Release -GNinja -L ../git
 echo
 
 # Perform the build
-make -j ${1:-1} -C ../release
+ninja -j 8 -C ../release
 echo
